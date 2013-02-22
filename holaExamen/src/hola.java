10f09c8ed0df10d0c42dc4a8e9fa6db7dd733373
cristoperalta <cristoph_lp@hotmail.com>
2013-02-22 20:10:12 +0000

    Rama3 v2

--- holaExamen/src/hola.java
+++ holaExamen/src/hola.java
@@ -7,6 +7,7 @@ public class hola {
 	//SOY OTRO USUARIOO
 	//¿Rama 2?
 	//¿Rama 3?
+	//¿Funcionó?
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.printf("Hola");
